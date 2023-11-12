e4c175db834d1a140ad9f8150c3bbad0ee6928a3
sukeshchand <sukesh.chand@hotmail.com>
2023-11-12 16:23:47 +0100

    add transaction database

---
+++ Databases/TransactionDatabase/dbo/Tables/IncomeExpenseSetup.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[IncomeExpenseSetup] (
+    [IncomeExpenseSetupId] INT            IDENTITY (1, 1) NOT NULL,
+    [StartDate]            DATETIME       NULL,
+    [EndDate]              DATETIME       NULL,
+    [IncomeExpenseType]    INT            NULL,
+    [Amount]               FLOAT (53)     NULL,
+    [Currency]             VARCHAR (50)   NULL,
+    [Descr]                NVARCHAR (200) NULL,
+    [NextPaymentDate]      DATETIME       NULL,
+    [NextBillingDate]      DATETIME       NULL,
+    [PaymentUrl]           NVARCHAR (500) NULL,
+    [AccountLogInUrl]      NVARCHAR (500) NULL,
+    CONSTRAINT [PK_IncomeExpenseSetup] PRIMARY KEY CLUSTERED ([IncomeExpenseSetupId] ASC)
+);
+
