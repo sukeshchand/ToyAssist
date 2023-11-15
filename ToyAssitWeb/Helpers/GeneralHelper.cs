8ab93e3e5fa9c6984640e62bf0e97e9188ea3961
sukeshchand <sukesh.chand@hotmail.com>
2023-11-15 19:21:39 +0100

    refactor and new table design

---
+++ ToyAssitWeb/Helpers/GeneralHelper.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using ToyAssist.Web.DatabaseModels;
+
+namespace ToyAssist.Web.Helpers
+{
+    public class GeneralHelper
+    {
+        public static string GetMonthAndDaysLeftString(DateTime? date)
+        {
+            if (date == null) return string.Empty;
+
+            var diff = (date - DateTime.Now).Value;
+            if (diff.TotalDays > 30)
+            {
+                var months = (int)(diff.TotalDays / 30);
+                var days = (int)(diff.TotalDays - (months * 30));
+                return $"{months} months {days} days left";
+            }
+            return $"{(int)diff.TotalDays} days left";
+        }
+
+        public static List<(string Name, PropertyInfo PropertyInfo)> GetProperties(Object obj)
+        {
+            var properties = new List<(string, PropertyInfo)>();
+            // Get the type of the object
+            Type type = obj.GetType();
+
+            // Get all public properties
+            PropertyInfo[] publicProperties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            Console.WriteLine("\nPublic Properties:");
+            foreach (var property in publicProperties)
+            {
+                properties.Add(new(property.Name, property));
+            }
+            return properties;
+        }
+
+        public string GenerateHtmlTable<T>(List<T> dataList, List<int> columnWidths)
+        {
+            try
+            {
+                if (dataList == null || dataList.Count == 0)
+                {
+                    return "<p>No data to display.</p>";
+                }
+
+                var properties = GetProperties(dataList[0]);
+
+                var tableHtml = new StringBuilder();
+
+                tableHtml.Append("<div class='table-responsive'><table class='table table-bordered'><thead><tr>");
+                int i = 0;
+                foreach (var property in properties)
+                {
+                    tableHtml.Append($"<th style='width: {columnWidths[i]}px;'>{property.Name}</th>");
+                    i++;
+                }
+
+                tableHtml.Append("</tr></thead><tbody>");
+                foreach (var item in dataList)
+                {
+                    tableHtml.Append("<tr>");
+                    i = 0;
+                    foreach (var property in properties)
+                    {
+                        tableHtml.Append($"<td><div style='max-width: {columnWidths[i]}px; overflow: auto; max-height: 100px;' >{property.PropertyInfo.GetValue(item)}</div></td>");
+                        i++;
+                    }
+                    tableHtml.Append("</tr>");
+                }
+
+                tableHtml.Append("</tbody></table></div>");
+
+                return tableHtml.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                return string.Empty;
+            }
+        }
+
+
+        public static List<T> ExecuteSQL<T>(DataContext dataContext, string sql)
+        {
+            FormattableString sqlExec = FormattableStringFactory.Create(sql);
+            return dataContext.Database.SqlQuery<T>(sqlExec).ToList();
+        }
+    }
+}
--- ToyAssitWeb/Pages/Index.razor.cs
+++ ToyAssitWeb/Pages/Index.razor.cs
@@ -42,10 +42,10 @@ namespace ToyAssist.Web.Pages
 
             _dataContext = new DataContext(options);
 
-            var incomeExpenseSetups = ExecuteSQL<IncomeExpenseSetup>("SELECT IncomeExpenseSetupId, StartDate, EndDate, IncomeExpenseType, Amount, Currency, Descr, NextPaymentDate, NextBillingDate, PaymentUrl, AccountLogInUrl FROM IncomeExpenseSetup");
+            var incomeExpenseSetups = GeneralHelper.ExecuteSQL<IncomeExpenseSetup>(_dataContext, "SELECT IncomeExpenseSetupId, StartDate, EndDate, IncomeExpenseType, Amount, Currency, Descr, NextPaymentDate, NextBillingDate, PaymentUrl, AccountLogInUrl FROM IncomeExpenseSetup");
             IncomeExpenseSetups.AddRange(incomeExpenseSetups);
 
-            var currencyConversionRates = ExecuteSQL<CurrencyConversionRate>("SELECT BaseCurrency, ToCurrency, ConversionRate FROM [dbo].[CurrencyConversionRate]");
+            var currencyConversionRates = GeneralHelper.ExecuteSQL<CurrencyConversionRate>(_dataContext, "SELECT BaseCurrency, ToCurrency, ConversionRate FROM [dbo].[CurrencyConversionRate]");
             CurrencyConversionRates.AddRange(currencyConversionRates);
 
             Month1ButtonLabel = DateTime.Now.ToString("MMM");
@@ -94,88 +94,7 @@ namespace ToyAssist.Web.Pages
             return conversionList;
         }
 
-        public string GetMonthAndDaysLeftString(DateTime? date)
-        {
-            if (date == null) return string.Empty;
-
-            var diff = (date - DateTime.Now).Value;
-            if (diff.TotalDays > 30)
-            {
-                var months = (int)(diff.TotalDays / 30);
-                var days = (int)(diff.TotalDays - (months * 30));
-                return $"{months} months {days} days left";
-            }
-            return $"{(int)diff.TotalDays} days left";
-        }
-
-        public static List<(string Name, PropertyInfo PropertyInfo)> GetProperties(Object obj)
-        {
-            var properties = new List<(string, PropertyInfo)>();
-            // Get the type of the object
-            Type type = obj.GetType();
-
-            // Get all public properties
-            PropertyInfo[] publicProperties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-            Console.WriteLine("\nPublic Properties:");
-            foreach (var property in publicProperties)
-            {
-                properties.Add(new(property.Name, property));
-            }
-            return properties;
-        }
-
-        public string GenerateHtmlTable<T>(List<T> dataList, List<int> columnWidths)
-        {
-            try
-            {
-                if (dataList == null || dataList.Count == 0)
-                {
-                    return "<p>No data to display.</p>";
-                }
-
-                var properties = GetProperties(dataList[0]);
-
-                var tableHtml = new StringBuilder();
-
-                tableHtml.Append("<div class='table-responsive'><table class='table table-bordered'><thead><tr>");
-                int i = 0;
-                foreach (var property in properties)
-                {
-                    tableHtml.Append($"<th style='width: {columnWidths[i]}px;'>{property.Name}</th>");
-                    i++;
-                }
-
-                tableHtml.Append("</tr></thead><tbody>");
-                foreach (var item in dataList)
-                {
-                    tableHtml.Append("<tr>");
-                    i = 0;
-                    foreach (var property in properties)
-                    {
-                        tableHtml.Append($"<td><div style='max-width: {columnWidths[i]}px; overflow: auto; max-height: 100px;' >{property.PropertyInfo.GetValue(item)}</div></td>");
-                        i++;
-                    }
-                    tableHtml.Append("</tr>");
-                }
-
-                tableHtml.Append("</tbody></table></div>");
-
-                return tableHtml.ToString();
-            }
-            catch (Exception ex)
-            {
-
-                return string.Empty;
-            }
-        }
-
-
-        private List<T> ExecuteSQL<T>(string sql)
-        {
-            FormattableString sqlExec = FormattableStringFactory.Create(sql);
-            return _dataContext.Database.SqlQuery<T>(sqlExec).ToList();
-        }
+      
 
         private async Task btnExpenseSettings_Click(MouseEventArgs e, int monthIndex)
         {
