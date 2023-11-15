7c1a30760d35ebcf5790ef7dab606e1747774568
sukeshchand <sukesh.chand@hotmail.com>
2023-11-15 19:33:58 +0100

    exp

---
+++ ToyAssitWeb/Pages/ExpenseSetup.razor.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using ToyAssist.Web.DatabaseModels;
+using ToyAssist.Web.Helpers;
+
+namespace ToyAssist.Web.Pages
+{
+
+    public partial class ExpenseSetup
+    {
+        public string ConnectionString;
+        private List<string> results = new List<string>();
+
+        public IConfiguration _configuration { get; }
+
+        List<IncomeExpenseSetup> IncomeExpenseSetups = new List<IncomeExpenseSetup>();
+        List<CurrencyConversionRate> CurrencyConversionRates = new List<CurrencyConversionRate>();
+        private readonly DataContext _dataContext;
+
+        public string Month1ButtonLabel { get; set; }
+        public string Month2ButtonLabel { get; set; }
+        public string Month3ButtonLabel { get; set; }
+        public string Month4ButtonLabel { get; set; }
+
+
+        public ExpenseSetup()
+        {
+            ConnectionString = (new ConfigurationReader()).GetSetting("ConnectionStrings:MainConnection");
+
+            var options = SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder<DataContext>(), ConnectionString).Options;
+
+            _dataContext = new DataContext(options);
+
+            var incomeExpenseSetups = GeneralHelper.ExecuteSQL<IncomeExpenseSetup>(_dataContext, "SELECT IncomeExpenseSetupId, StartDate, EndDate, IncomeExpenseType, Amount, Currency, Descr, NextPaymentDate, NextBillingDate, PaymentUrl, AccountLogInUrl FROM IncomeExpenseSetup");
+            IncomeExpenseSetups.AddRange(incomeExpenseSetups);
+
+            var currencyConversionRates = GeneralHelper.ExecuteSQL<CurrencyConversionRate>(_dataContext, "SELECT BaseCurrency, ToCurrency, ConversionRate FROM [dbo].[CurrencyConversionRate]");
+            CurrencyConversionRates.AddRange(currencyConversionRates);
+
+            Month1ButtonLabel = DateTime.Now.ToString("MMM");
+            Month2ButtonLabel = FirstDayOfNextMonth(1).ToString("MMM");
+            Month3ButtonLabel = FirstDayOfNextMonth(2).ToString("MMM");
+            Month4ButtonLabel = FirstDayOfNextMonth(3).ToString("MMM");
+        }
+
+        public DateTime FirstDayOfNextMonth(int nextMonthAfter)
+        {
+            DateTime today = DateTime.Now;
+            if (nextMonthAfter > 1)
+            {
+                today = FirstDayOfNextMonth(nextMonthAfter - 1);
+            }
+
+            // Find the last day of the current month
+            DateTime lastDayOfMonth = new DateTime(today.Year, today.Month, DateTime.DaysInMonth(today.Year, today.Month));
+
+            // Find the first day of the next month
+            DateTime firstDayOfNextMonth = lastDayOfMonth.AddDays(1);
+            return firstDayOfNextMonth;
+
+        }
+
+        public List<string> GetConversionList(string baseCurrency, double amount)
+        {
+            var currenciesInUse = IncomeExpenseSetups.Select(x => x.Currency).Distinct().ToList();
+            var conversionList = new List<string>();
+            foreach (var currency in currenciesInUse)
+            {
+                var conversionRate = CurrencyConversionRates.FirstOrDefault(x => x.BaseCurrency == baseCurrency && x.ToCurrency == currency);
+                if (conversionRate != null)
+                {
+                    conversionList.Add($"{currency} {(int)(amount * (double)conversionRate.ConversionRate)}");
+                }
+                else
+                {
+                    var conversionRateReverse = CurrencyConversionRates.FirstOrDefault(x => x.BaseCurrency == currency && x.ToCurrency == baseCurrency);
+                    if (conversionRateReverse != null)
+                    {
+                        conversionList.Add($"{currency} {(int)(amount / (double)conversionRateReverse.ConversionRate)}");
+                    }
+                }
+            }
+            return conversionList;
+        }
+
+    }
+}
