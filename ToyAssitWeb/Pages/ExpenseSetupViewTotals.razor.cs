449030110527659ec5ed0ef2fdf87d88b8df5c8d
Sukesh Chand <sukesh.chand@3bits.se>
2023-12-20 16:19:07 +0100

    expense setup view total

---
+++ ToyAssitWeb/Pages/ExpenseSetupViewTotals.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+using BlazorBootstrap;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+using ToyAssist.Web.DatabaseModels.Models;
+using ToyAssist.Web.Factories;
+using ToyAssist.Web.ViewModels;
+
+namespace ToyAssist.Web.Pages
+{
+
+    public partial class ExpenseSetupViewTotals
+    {
+        [Parameter]
+        public required ExpenseSetup ModalData { get; set; }
+    }
+}
