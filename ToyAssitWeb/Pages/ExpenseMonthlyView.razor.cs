9256756f5cef77de58e09aa3e57b26b5cd688cc8
sukeshchand <sukesh.chand@hotmail.com>
2023-12-21 18:19:07 +0100

    add new iew

---
+++ ToyAssitWeb/Pages/ExpenseMonthlyView.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+using BlazorBootstrap;
+
+using Microsoft.EntityFrameworkCore;
+
+using ToyAssist.Web.DatabaseModels.Models;
+using ToyAssist.Web.Factories;
+using ToyAssist.Web.ViewModels;
+
+namespace ToyAssist.Web.Pages
+{
+
+    public partial class ExpenseMonthlyView
+    {
+
+        List<ExpenseSetup> ExpenseSetups = new List<ExpenseSetup>();
+        List<CurrencyConversionRate> CurrencyConversionRates = new List<CurrencyConversionRate>();
+        public bool IsPostBack { get; set; }
+
+        public ExpenseMonthlyView()
+        {
+        }
+    }
+}
